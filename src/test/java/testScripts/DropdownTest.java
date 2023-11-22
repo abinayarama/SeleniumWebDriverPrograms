c4f1047b91a9fc6904a39b16dbccacff92cf676b
abinayarama <abinayaramachandran88@gmail.com>
2023-11-22 10:50:31 +0000

    dropdown test is added

---
+++ src/test/java/testScripts/DropdownTest.java
@@ -0,0 +1,16 @@
+package testScripts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DropdownTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://demo.seleniumeasy.com/basic-select-dropdown-demo.html");
+		
+
+	}
+
+}
